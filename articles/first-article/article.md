545ad710abbf776f738b86f69cff9aefe01b365e
chirprush <chirpyboat73@gmail.com>
2024-07-24 22:44:07 -0400

    A start to static generation and reviving the website

---
+++ articles/first-article/article.md
@@ -0,0 +1,29 @@
+# First Article
+
+This is mainly just a test article to see if my custom static website generator
+(a wrapper over Pandoc, how unique) works!
+
+But since you're here, I might as well lay out what I intend to write here:
+
+- **Math stuff**: There's much to explore and learn in the realm of
+mathematics, and I've barely scratched the surface when it comes to the higher
+level stuff. I want to learn and write a whole bunch about group theory,
+topology perhaps, number theory, combinatorics, basically anything that I
+find interesting.
+- **Comp Sci stuff**: In a similar vein to mathematics, there's so much I have
+to learn and talk about when it comes to comp sci stuff. There's data
+structures/algorithms, but also really cool niche stuff like SIMD and shader
+programming and all that. So yeah, there's a lot I want to hopefully cover
+- **Japanese stuff**: I'm not actively studying Japanese much anymore (moreso
+just going with immersion), but it could be nice to write down some
+musings or just what I like about the grammar.
+- **Life musings**: It's not a blog if I'm not philosophical y'know.
+
+\[
+    \textrm{Sugar} \longrightarrow \textrm{Me} \longrightarrow \textrm{Writing}
+.\]
+
+No promises on when I write stuff; I'd like to ideally strike a balance between
+not feeling pressured to write but also at the very least writing a bit every
+once in a while. After I have a whole bunch of content, it'll be fun to see what
+I can do with it (visualizations like graphs of links between articles, etc.).
---
+++ render.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import subprocess
+import yaml
+from pathlib import Path
+
+# TODO: Allow for specifying templates within the yaml file
+# (doesn't take long at all to implement; this is just a note)
+class Article:
+    def __init__(self, base):
+        self.base = base
+        self.article = base / "article.md"
+        self.metadata = base / "meta.yaml"
+        self.result = base / "index.html"
+
+        if not self.article.exists():
+            raise ValueError(f"   Could not find article markdown file at '{self.article}'")
+
+        if not self.metadata.exists():
+            raise ValueError(f"   Could not find yaml metadata file at '{self.metadata}'")
+
+        with open(str(self.metadata.absolute())) as stream:
+            try:
+                self.data = yaml.safe_load(stream)
+
+                self.name = self.data["title"]
+            except yaml.YAMLError as e:
+                raise ValueError("   Error in parsing yaml:\n\n" + str(e))
+            except KeyError as e:
+                raise ValueError("   Title not listed for article")
+
+
+    def render(self, log):
+        # Tweaking the options to get the correct arrangement for Katex to actually render
+        # the correct stuff while also using the \( \) and \[ \] characters for latex in 
+        # markdown instead of the not very nice $ $ and $$ $$ delimiters actually took
+        # really long (like O(2^n) time :sob:).
+
+        # Yeah unfortunately even though we're using Katex to do the actual rendering,
+        # if you use the katex flag instead of the mathjax flag, it won't work :(
+        # Thank you pandoc, very cool
+        cmd = ["pandoc", "-f", "markdown-smart+tex_math_single_backslash", "--standalone", "--mathjax", "--template=templates/template.html", "--metadata-file=" + str(self.metadata), "-o", str(self.result), str(self.article)]
+        print()
+        print("   " + " ".join(cmd[:3]))
+        print("   " + " ".join(cmd[3:]))
+
+        log.write(f"Command: {' '.join(cmd)}\n")
+
+        result = subprocess.run(cmd, text=True, capture_output=True)
+
+        log.write(f"Return code: {result.returncode}\n")
+        log.write(f"Stdout:\n")
+        log.write(result.stdout + "\n")
+
+        log.write(f"Stderr:\n")
+        log.write(result.stderr + "\n")
+
+        if result.returncode != 0:
+            print(f"   Failed with return code {result.returncode}, see log for more details")
+        else:
+            print(f"   Succeeded")
+
+# Main pipeline:
+# - Run pandoc for the main pages -> index.html, articles.html, about.html (to be decided I guess), resume.html
+#   - Overall I'm kinda iffy on whether or not I really want to render the main pages since I might want to hardcode stuff for those
+#     on one hand it's more extendable and there's repetition if I ever want to change something (which honestly could happen a lot),
+#     but if I want to customize stuff with like say a p5 canvas or something I have to hardcode that in which is like awkward.
+#     I guess articles.html should be generated though.
+# - Run pandoc on each of the articles and stuff
+# - Throughout the proper variables should be given for each thing (variables for like the selection bar and stuff)
+
+# Eventually I'll have to deal with fixing the css and making things responsive
+# as well, but I'll work on that once I actually get content and can see how
+# things look I suppose
+
+articles_dir = Path("./articles")
+articles = [Article(d) for d in articles_dir.iterdir() if d.is_dir()]
+
+with open("log.txt", "w") as log:
+    for d in articles_dir.iterdir():
+        if not d.is_dir():
+            continue
+
+        print(f"-> Retrieving article at '{d}'")
+
+        try:
+            article = Article(d)
+        except ValueError as e:
+            print(e)
+            print("   Skipping..")
+            continue
+
+        print(f"   Rendering article '{article.name}' to '{article.result}'")
+
+        log.write(f"Rendering article at '{d}'\n")
+        article.render(log)
+
+        print()
